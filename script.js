c75783630fa31798cc1a13e4a7a2a52681dab4f6
Ritta <brittanykelly33@gmail.com>
2022-05-09 16:20:25 -0400

    added in additional fetch req

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=7')
+    fetch('https://pokeapi.co/api/v2/pokemon/1')
+        .then(resp => resp.json())
+        .then((data) => console.log(data));
+
+    fetch('https://pokeapi.co/api/v2/pokemon/4')
+        .then(resp => resp.json())
+        .then((data) => console.log(data));
+
+    fetch('https://pokeapi.co/api/v2/pokemon/7')
         .then(resp => resp.json())
         .then((data) => console.log(data))
 })
