e1a33c846b5b733550e5f7fb37d8ad855addc0ee
brian corker <bcorker123@gmail.com>
2022-05-10 18:08:55 -0400

    fixed minor errors

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ const renderPoke = (obj, id) => {
 }
 
 const battlePoke = (yourId, rivalId, obj) => {
+    console.log(obj)
+
     const addClick = (move) => {
+        console.log(move)
         move.addEventListener('click', ()=>{
             // adding text under your div that says what move you used
             const yourPokeDiv = document.querySelector('#yourPokeDiv')
@@ -42,6 +45,7 @@ const battlePoke = (yourId, rivalId, obj) => {
                 return Math.floor(Math.random() * (max - min + 1) + min)
             }
             const rndInt = randomIntFromInterval(0, 3)
+            console.log(rndInt)
             const rivalMove = rivalMovesArray[rndInt].textContent
 
             const rivalPokeDiv = document.querySelector('#rivalPokeDiv')
@@ -65,66 +69,65 @@ const battlePoke = (yourId, rivalId, obj) => {
   yourImg.className = 'choosePkmn'
   yourImg.id = 'yourPkmnBattler'
 
-  const rivalImg = document.createElement('img')
-  rivalImg.src = obj[rivalId].img
-  rivalImg.className = 'choosePkmn'
+    const rivalImg = document.createElement('img')
+    rivalImg.src = obj[rivalId].img
+    rivalImg.className = 'choosePkmn'
   rivalImg.id = 'rivalPkmnBattler'
 
-  const yourName = document.querySelector('#yourPokeName')
-  yourName.textContent = obj[yourId].name
-  yourName.className = 'pkmn'
-  yourName.append(yourImg)
+    const yourName = document.querySelector('#yourPokeName')
+    yourName.textContent = obj[yourId].name
+    yourName.className = 'pkmn'
+    yourName.append(yourImg)
 
-  const yourMove1 = document.querySelector('#yourPokeMove1')
-  yourMove1.textContent = obj[yourId].moves[0]
+    const yourMove1 = document.querySelector('#yourPokeMove1')
+    yourMove1.textContent = obj[yourId].moves[0]
 
-  const yourMove2 = document.querySelector('#yourPokeMove2')
-  yourMove2.textContent = obj[yourId].moves[1]
+    const yourMove2 = document.querySelector('#yourPokeMove2')
+    yourMove2.textContent = obj[yourId].moves[1]
 
-  const yourMove3 = document.querySelector('#yourPokeMove3')
-  yourMove3.textContent = obj[yourId].moves[2]
+    const yourMove3 = document.querySelector('#yourPokeMove3')
+    yourMove3.textContent = obj[yourId].moves[2]
 
-  const yourMove4 = document.querySelector('#yourPokeMove4')
-  yourMove4.textContent = obj[yourId].moves[3]
+    const yourMove4 = document.querySelector('#yourPokeMove4')
+    yourMove4.textContent = obj[yourId].moves[3]
 
-  const rivalName = document.querySelector('#rivalPokeName')
-  rivalName.textContent = obj[rivalId].name
-  rivalName.className = 'pkmn'
-  rivalName.append(rivalImg)
+    const rivalName = document.querySelector('#rivalPokeName')
+    rivalName.textContent = obj[rivalId].name
+    rivalName.className = 'pkmn'
+    rivalName.append(rivalImg)
 
-  const rivalMove1 = document.querySelector('#rivalPokeMove1')
-  rivalMove1.textContent = obj[rivalId].moves[0]
+    const rivalMove1 = document.querySelector('#rivalPokeMove1')
+    rivalMove1.textContent = obj[rivalId].moves[0]
 
-  const rivalMove2 = document.querySelector('#rivalPokeMove2')
-  rivalMove2.textContent = obj[rivalId].moves[1]
+    const rivalMove2 = document.querySelector('#rivalPokeMove2')
+    rivalMove2.textContent = obj[rivalId].moves[1]
 
-  const rivalMove3 = document.querySelector('#rivalPokeMove3')
-  rivalMove3.textContent = obj[rivalId].moves[2]
+    const rivalMove3 = document.querySelector('#rivalPokeMove3')
+    rivalMove3.textContent = obj[rivalId].moves[2]
 
-  const rivalMove4 = document.querySelector('#rivalPokeMove4')
-  rivalMove4.textContent = obj[rivalId].moves[3]
+    const rivalMove4 = document.querySelector('#rivalPokeMove4')
+    rivalMove4.textContent = obj[rivalId].moves[3]
 
-  const yourMovesArray = [yourMove1, yourMove2, yourMove3, yourMove4]
-  const rivalMovesArray = [rivalMove1, rivalMove2, rivalMove3, rivalMove4]
+    const yourMovesArray = [yourMove1, yourMove2, yourMove3, yourMove4]
+    const rivalMovesArray = [rivalMove1, rivalMove2, rivalMove3, rivalMove4]
 
-  yourMovesArray.forEach(move => addClick(move))
+    yourMovesArray.forEach(move=>addClick(move))
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const pokeObj = {}
-  const addPokeObj = (data) => {
-    pokeObj[data.id] = {
-      name: data.name,
-      img: data.sprites.front_default,
-      moves: [
-        data.moves[0].move.name,
-        data.moves[1].move.name,
-        data.moves[2].move.name,
-        data.moves[3].move.name,
-      ],
+    const pokeObj = {}
+    const addPokeObj = (data) => {
+        pokeObj[data.id] = {
+            name: data.name, 
+            img: data.sprites.front_default,
+            moves: [
+                data.moves[0].move.name,
+                data.moves[1].move.name,
+                data.moves[2].move.name,
+                data.moves[3].move.name,
+            ]
+        }
     }
-
-// previous fetch requests -- have to manually duplicate for each new pokemon
 //   fetch('https://pokeapi.co/api/v2/pokemon/1')
 //     .then((resp) => resp.json())
 //     .then((data) => {
@@ -151,6 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log(pokeObj)
 
+  //battlePoke(1, 4)
   document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
       console.log('I was reset')
@@ -158,12 +162,6 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector('#rivalPokeHP').innerText = 100
     }
   })
-
-  //form submission event handler (keep this commented out code!!!)
-
-
-
-
 
   // testing multiple fetch request loop
   let fetchArray = []
@@ -179,18 +177,20 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   loopFetch(9)
   
-  Promise.all(fetchArray)
-  .then(values=>{
-      for(const id in pokeObj) {renderPoke(pokeObj, id)}
-      // test => battlePoke(3, 6, pokeObj)
+    Promise.all(fetchArray)
+    .then(values=>{
+        for(const id in pokeObj) {renderPoke(pokeObj, id)}
+        //battlePoke(3, 6, pokeObj)
     })
 
-     const battleForm = document.querySelector('#choosePkmn')
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const yourInput = document.querySelector('#yourEntry').value
-    const rivalInput = document.querySelector('#rivalEntry').value
-    battlePoke(yourInput, rivalInput, pokeObj);
-  };
-  battleForm.addEventListener('submit', handleSubmit); 
+    const battleForm = document.querySelector('#choosePkmn')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const yourInput = document.querySelector('#yourEntry').value
+        const rivalInput = document.querySelector('#rivalEntry').value
+        battlePoke(yourInput, rivalInput, pokeObj);
+    };
+    battleForm.addEventListener('submit', handleSubmit); 
+
+
 })
